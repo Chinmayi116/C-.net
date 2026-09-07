d555ea18ac280b13bd789046fa9e63c6bec1a7f1
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-07 18:22:40 +0530

    Create Largest_of_Two_Numbers.cs

---
+++ Largest_of_Two_Numbers.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b)
+            Console.WriteLine(a + " is largest");
+        else
+            Console.WriteLine(b + " is largest");
+    }
+}
