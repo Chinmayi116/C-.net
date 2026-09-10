d6ddd22022e7367cca707944dad77823fac76367
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-10 15:56:00 +0530

    Implement conditional check using logical OR

---
+++ Multipl_Conditions_using_||.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 5;
+
+        if (num == 5 || num == 10)
+        {
+            Console.WriteLine("Number is 5 or 10");
+        }
+    }
+}
