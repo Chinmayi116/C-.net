63991527a7b73be300126b8611ddacccbeedad1c
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-10 15:56:54 +0530

    Create Multiple_Conditions_using_&&.cs

---
+++ Multiple_Conditions_using_&&.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 22;
+        int marks = 70;
+
+        if (age >= 18 && marks >= 50)
+        {
+            Console.WriteLine("Eligible");
+        }
+        else
+        {
+            Console.WriteLine("Not Eligible");
+        }
+    }
+}
