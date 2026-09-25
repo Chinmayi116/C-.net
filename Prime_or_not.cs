afe6132f6bae7b6497756fa99b7082423d324ece
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-25 18:10:40 +0530

    Add prime number checking program
    Implement a console application to check if a number is prime.

---
+++ Prime_or_not.cs
@@ -0,0 +1,17 @@
+Write a program which asks the user for an integer number and answers if it is a prime 
+number or not.
+static void Main(string[] args)
+ {
+int number;
+int dividers = 0;
+Console.Write("Enter a number: ");
+ number = Convert.ToInt32(Console.ReadLine());
+for (inti = 1; i<= number; i++)
+if (number % i == 0)
+ dividers++;
+if (dividers == 2)
+Console.WriteLine("The number is prime");
+else
+Console.WriteLine("The number is not prime");
+Console.Read();
+ }
