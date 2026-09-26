8a039872fe74414b11cfe8d26cd057016a20df3f
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-26 15:31:12 +0530

    Create Automatic_property.cs

---
+++ Automatic_property.cs
@@ -0,0 +1,14 @@
+class Person
+{
+ public string Name // property
+{ get; set; }
+}
+class Program
+{
+ static void Main(string[] args)
+ {
+ Person myObj = new Person();
+myObj.Name = "Liam";
+Console.WriteLine(myObj.Name);
+ }
+}
